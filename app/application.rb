fb9f556fe1200e4f09b752902c4fcf756e8f162e
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-20 18:31:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,10 +17,11 @@ class Application
     elsif req.path.match(/cart/)
       if @@cart.empty?
         resp.write "Your cart is empty"
-        
+      else
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
+      end
     elsif req.path.match(/add/)
       search_term = req.params["item"]
       resp.write handle_search(search_term)
