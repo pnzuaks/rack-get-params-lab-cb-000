da9326dbe3a526fe3d19395b317871ff42811cfe
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-20 18:40:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -29,7 +29,7 @@ class Application
       search_term = req.params["item"]
       resp.write handle_search(search_term)
             if @@items.include?(search_term) == false
-            @@cart.osearch_term
+            @@cart.push(search_term)
             resp.write "added #{search_term}"
             resp.write "#{search_term} is one of our items"
 
